05065ffbaa41b1489c7105eb3bb660486c19923b
Soter <tirsoclarineteharo@gmail.com>
2022-12-05 19:22:46 +0100

    tercera demo

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static(path.join(__dirname, '../build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
 // Inicialización del servidor
